6d58ee5b678a52224e7b2fc2458a1d489c93e973
unknown <henokmekuanint79@gmail.com>
2022-05-30 21:25:54 +0300

    rendering class added

--- basic.py
+++ basic.py
@@ -1,84 +1,68 @@
+from pickle import OBJ
 import glfw
 from OpenGL.GL import *
-import OpenGL.GL.shaders
+import ShaderLoader
 import numpy
 import pyrr
+from PIL import Image
+import OBJ
 
+def window_resize(window,width, height):
+    glViewport(0, 0, width, height)
 
 def main():
 
+    # initialize glfw
     if not glfw.init():
         return
 
-    window = glfw.create_window(800, 600, "My OpenGL window", None, None)
+    w_width, w_height = 800, 600
+
+    #glfw.window_hint(glfw.RESIZABLE, GL_FALSE)
+
+    window = glfw.create_window(w_width, w_height, "My OpenGL window", None, None)
 
     if not window:
         glfw.terminate()
         return
 
     glfw.make_context_current(window)
-    #        positions        colors
-    cube = [-0.5, -0.5,  0.5, 1.0, 0.0, 0.0,
-             0.5, -0.5,  0.5, 0.0, 1.0, 0.0,
-             0.5,  0.5,  0.5, 0.0, 0.0, 1.0,
-            -0.5,  0.5,  0.5, 1.0, 1.0, 1.0,
+    glfw.set_window_size_callback(window, window_resize)
 
-            -0.5, -0.5, -0.5, 1.0, 0.0, 0.0,
-             0.5, -0.5, -0.5, 0.0, 1.0, 0.0,
-             0.5,  0.5, -0.5, 0.0, 0.0, 1.0,
-            -0.5,  0.5, -0.5, 1.0, 1.0, 1.0]
+    obj = OBJ.ObjLoader()
+    obj.load_model("blackpices/chessboardobj.obj")
 
-    cube = numpy.array(cube, dtype = numpy.float32)
+    texture_offset = len(obj.vertex_index)*12
 
-    indices = [0, 1, 2, 2, 3, 0,
-               4, 5, 6, 6, 7, 4,
-               4, 5, 1, 1, 0, 4,
-               6, 7, 3, 3, 2, 6,
-               5, 6, 2, 2, 1, 5,
-               7, 4, 0, 0, 3, 7]
 
-    indices = numpy.array(indices, dtype= numpy.uint32)
-
-    vertex_shader = """
-    #version 330
-    in vec3 position;
-    in vec3 color;
-    uniform mat4 transform;
-    out vec3 newColor;
-    void main()
-    {
-        gl_Position = transform * vec4(position, 1.0f);
-        newColor = color;
-    }
-    """
-
-    fragment_shader = """
-    #version 330
-    in vec3 newColor;
-    out vec4 outColor;
-    void main()
-    {
-        outColor = vec4(newColor, 1.0f);
-    }
-    """
-    shader = OpenGL.GL.shaders.compileProgram(OpenGL.GL.shaders.compileShader(vertex_shader, GL_VERTEX_SHADER),
-                                              OpenGL.GL.shaders.compileShader(fragment_shader, GL_FRAGMENT_SHADER))
+    shader = ShaderLoader.compile_shader("shaders/video_17_vert.vs", "shaders/video_17_frag.fs")
 
     VBO = glGenBuffers(1)
     glBindBuffer(GL_ARRAY_BUFFER, VBO)
-    glBufferData(GL_ARRAY_BUFFER, 192, cube, GL_STATIC_DRAW)
+    glBufferData(GL_ARRAY_BUFFER, obj.model.itemsize * len(obj.model), obj.model, GL_STATIC_DRAW)
 
-    EBO = glGenBuffers(1)
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO)
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 144, indices, GL_STATIC_DRAW)
+    #position
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, obj.model.itemsize * 3, ctypes.c_void_p(0))
+    glEnableVertexAttribArray(0)
+    #texture
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, obj.model.itemsize * 2, ctypes.c_void_p(texture_offset))
+    glEnableVertexAttribArray(1)
 
-    position = glGetAttribLocation(shader, "position")
-    glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
-    glEnableVertexAttribArray(position)
 
-    color = glGetAttribLocation(shader, "color")
-    glVertexAttribPointer(color, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
-    glEnableVertexAttribArray(color)
+    texture = glGenTextures(1)
+    glBindTexture(GL_TEXTURE_2D, texture)
+    # Set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    # Set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    # load image
+    image = Image.open("res/boardtexture.jpg")
+    flipped_image = image.transpose(Image.FLIP_TOP_BOTTOM)
+    img_data = numpy.array(list(flipped_image.getdata()), numpy.uint8)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.width, image.height, 0, GL_RGB, GL_UNSIGNED_BYTE, img_data)
+    glEnable(GL_TEXTURE_2D)
 
 
     glUseProgram(shader)
@@ -87,18 +71,32 @@ def main():
     glEnable(GL_DEPTH_TEST)
     #glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
 
+    view = pyrr.matrix44.create_from_translation(pyrr.Vector3([0.0, 0.0, -3.0]))
+    projection = pyrr.matrix44.create_perspective_projection_matrix(65.0, w_width / w_height, 0.1, 100.0)
+    model = pyrr.matrix44.create_from_translation(pyrr.Vector3([0.0, 0.0, 0.0]))
+
+    view_loc = glGetUniformLocation(shader, "view")
+    proj_loc = glGetUniformLocation(shader, "projection")
+    model_loc = glGetUniformLocation(shader, "model")
+
+    glUniformMatrix4fv(view_loc, 1, GL_FALSE, view)
+    glUniformMatrix4fv(proj_loc, 1, GL_FALSE, projection)
+    glUniformMatrix4fv(model_loc, 1, GL_FALSE, model)
+
+
+
     while not glfw.window_should_close(window):
         glfw.poll_events()
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-        rot_x = pyrr.Matrix44.from_x_rotation(0.5 )
-        rot_y = pyrr.Matrix44.from_y_rotation(0.8)
+        rot_x = pyrr.Matrix44.from_x_rotation(-0.5)
+        rot_y = pyrr.Matrix44.from_y_rotation(-0.5)
 
         transformLoc = glGetUniformLocation(shader, "transform")
         glUniformMatrix4fv(transformLoc, 1, GL_FALSE, rot_x * rot_y)
 
-        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, None)
+        glDrawArrays(GL_TRIANGLES, 0, len(obj.vertex_index))
 
         glfw.swap_buffers(window)
 
